3f9f8c6ef7b10cbe24bfea1da85e77dca9d44f4d
ibarczew@cisco.com <ian.barczewski@wwt.com>
2021-08-03 13:38:19 -0700

    base checkout page built

---
+++ server.js
@@ -0,0 +1,26 @@
+const stripe = require("stripe")(
+  "sk_test_51JKTsvIBX7TlKpWByria43yOJ0T8cxBw0SirF8uteDrb4xz11qHLSQQtbJUvN9djiavDN5QToi1GBInWRZdCtBip00vgVBZ3EO"
+);
+const express = require("express");
+const app = express();
+app.use(express.static("."));
+
+const domain = "http://localhost:3000";
+
+app.post("/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price: "price_1JKU4sIBX7TlKpWBpCoFf9uP",
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `${domain}/success.html`,
+    cancel_url: `${domain}/cancel.html`,
+  });
+
+  res.redirect(303, session.url);
+});
+app.listen(3000, () => console.log('Running on port 3000'));
